c603c2c1939afac6af298010f7d2b063cb799984
fjavaler <conscientiadivinus@gmail.com>
2017-04-01 10:13:31 -0600

    Created local Git repo

---
+++ Serialization1/src/m5_a2/Person.java
@@ -0,0 +1,135 @@
+/********************************************************
+ * Project : Serialization I
+ * File : Person.java 
+ * Name: Frederick Javalera 
+ * Date : 3/28/17 (3/29/17)>
+ *
+ * Description : (Narrative description, not code)
+ *
+ * 1) What is the purpose of the code; what problem does the code solve.
+ * The purpose of this code is to create Person objects. A person object
+ * contains a name and a cell phone number.
+ *
+ * 2) What data-structures are used.
+ * No specific data structures used.
+ *
+ * 3) What algorithms, techniques, etc. are used in implementing the data
+ * structures.
+ * None.
+ *
+ * 4) What methods are implemented (optional).
+ *
+ * Changes : <Description|date of modifications>
+ ********************************************************/
+package m5_a2;
+
+import java.io.Serializable;
+
+/**
+ * This class creates Person objects. A person object contains 
+ * a name and a cell phone number.
+ * 
+ * @author Frederick Javalera
+ */
+public class Person implements Serializable
+{
+	// fields
+	private String name = "";
+	private String cellPhone = "";
+
+	/****************************************************
+	 * Method : Person (constructor)
+	 *
+	 * Purpose : The constructor method constructs a Person object.
+	 *
+	 * Parameters : _name - The name of the Person.
+	 * _cellPhone         - The cell Person's cell phone number.
+	 *
+	 * Returns : This method does not return a value.
+	 *
+	 ****************************************************/
+	public Person(String _name, String _cellPhone)
+	{
+		this.name = _name;
+		this.cellPhone = _cellPhone;
+	}
+
+	/****************************************************
+	 * Method : getName
+	 *
+	 * Purpose : The getName method returns the name of the person.
+	 *
+	 * Parameters : None.
+	 *
+	 * Returns : Returns the name of the person.
+	 *
+	 ****************************************************/
+	public String getName()
+	{
+		return name;
+	}
+
+	/****************************************************
+	 * Method : setName
+	 *
+	 * Purpose : The setName method updates the person's name to name.
+	 *
+	 * Parameters : name - the name to be set to.
+	 *
+	 * Returns : This method does not return a value.
+	 *
+	 ****************************************************/
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/****************************************************
+	 * Method : getCellPhone
+	 *
+	 * Purpose : The getCellPhone method returns the person's cell phone
+	 * number.
+	 *
+	 * Parameters : None.
+	 *
+	 * Returns : This method returns the person's cell phone number.
+	 *
+	 ****************************************************/
+	public String getCellPhone()
+	{
+		return cellPhone;
+	}
+
+	/****************************************************
+	 * Method : setCellPhone
+	 *
+	 * Purpose : The setCellPhone method sets the cellPhone field to cellPhone.
+	 *
+	 * Parameters : cellPhone - The cell phone number to be set to.
+	 *
+	 * Returns : This method does not return a value.
+	 *
+	 ****************************************************/
+	public void setCellPhone(String cellPhone)
+	{
+		this.cellPhone = cellPhone;
+	}
+
+	/****************************************************
+	 * Method : toString
+	 *
+	 * Purpose : The toString method returns a string containing the 
+	 * person's name and cell phone number.
+	 *
+	 * Parameters : None.
+	 *
+	 * Returns : This method returns a string containing the person's name 
+	 * and cell phone number.
+	 *
+	 ****************************************************/
+	@Override
+	public String toString()
+	{
+		return name + ", " + cellPhone;
+	}
+}
---
+++ Serialization1/src/m5_a2/PersonClient.java
@@ -0,0 +1,137 @@
+/********************************************************
+ * Project : Serialization I 
+ * File : PersonClient.java 
+ * Name: Frederick Javalera 
+ * Date : 3/28/17 (3/29/17)
+ *
+ * Description : (Narrative description, not code)
+ *
+ * 1) What is the purpose of the code; what problem does the code solve.
+ * The purpose of this code is to act as a client which creates a list of
+ * Person objects, writes that list in serialized form to a .ser doc, and
+ * reads that doc back in and prints the results. The original list and
+ * the returned list should be the same.
+ *
+ * 2) What data-structures are used.
+ * ArrayLists are used in this program.
+ *
+ * 3) What algorithms, techniques, etc. are used in implementing the data
+ * structures.
+ * An enhanced for loop is used to print the beginning and end results.
+ *
+ * 4) What methods are implemented (optional).
+ *
+ * Changes : <Description|date of modifications>
+ ********************************************************/
+package m5_a2;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ * This class acts as a client which creates a list of Person objects, 
+ * writes that list in serialized form to a .ser doc, and reads that 
+ * doc back in and prints the results. The original list and the 
+ * returned list should be the same.
+ * 
+ * @author Frederick Javalera
+ */
+public class PersonClient
+{
+	/****************************************************
+	 * Method : main
+	 *
+	 * Purpose : The main method creates an ArrayList of Persons and prints them
+	 * out. Next, it serializes the list. It then de-serializes the list and prints
+	 * the de-serialized resulting list. The two lists should be identical.
+	 *
+	 * Parameters : args - An array of arguments.
+	 *
+	 * Returns : This method does not return a value.
+	 *
+	 ****************************************************/
+	public static void main(String[] args)
+	{
+		ArrayList<Person> personList = new ArrayList<Person>();
+		personList.add(new Person("Joe Schmoe", "8018671234"));
+		personList.add(new Person("Blow Schmoe", "8018671235"));
+		personList.add(new Person("Know Schmoe", "8018671236"));
+		personList.add(new Person("Dough Schmoe", "8018671237"));
+		personList.add(new Person("Snow Schmoe", "8018671238"));
+
+		System.out.println("Person list before writing: ");
+		for (Person p : personList)
+		{
+			System.out.println(p);
+		}
+		System.out.println("\n");
+
+		PersonClient client = new PersonClient();
+		client.serialize(personList, "src/m5_a2/Persons.ser");
+		System.out.println("Serialization done...");
+		ArrayList<Person> newList = client.deserialize("src/m5_a2/Persons.ser");
+		System.out.println("De-serialization done...");
+		System.out.println("\n");
+		
+		System.out.println("Person list after reading: ");
+		for (Person p : personList)
+		{
+			System.out.println(p);
+		}
+		System.out.println("\n");
+	}
+
+	/****************************************************
+	 * Method : serialize
+	 *
+	 * Purpose : The serialize method serializes pList and writes it to file, fileName.
+	 *
+	 * Parameters : pList - An arrayList of Person objects.
+	 * fileName           - The name and location of the file to be written.
+	 *
+	 * Returns : This method does not return a value.
+	 *
+	 ****************************************************/
+	public void serialize(ArrayList<Person> pList, String fileName)
+	{
+		try (ObjectOutputStream outStream = new ObjectOutputStream(new FileOutputStream(fileName)))
+		{
+			outStream.writeObject(pList);
+		}
+		catch (IOException e)
+		{
+			System.out.println("A problem occurred during serialization.");
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/****************************************************
+	 * Method : deserialize
+	 *
+	 * Purpose : The deserialize method reads the file located at fileName
+	 * and stores its contents in a Person ArrayList which it then returns.
+	 *
+	 * Parameters : fileName - The name and location of the file to be written.
+	 *
+	 * Returns : This method returns an ArrayList of Persons.
+	 *
+	 ****************************************************/
+	public ArrayList<Person> deserialize(String fileName)
+	{
+		ArrayList<Person> pList = new ArrayList<Person>();
+		try (ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(fileName)))
+		{
+			pList = (ArrayList<Person>) inStream.readObject();
+		}
+		catch (IOException | ClassNotFoundException e)
+		{
+			System.out.println("A problem occurred during de-serialization.");
+			System.out.println(e.getMessage());
+		}
+		return pList;
+	}
+}
